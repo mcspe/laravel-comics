7eb6e50c89e87cb7f2a0aee6b3c9c9e9ba695248
mcspe <mcspezzaferro@gmail.com>
2023-06-05 22:56:49 +0200

    [add] footer menus

--- config/menus.php
+++ config/menus.php
@@ -41,5 +41,115 @@
                 'label'=>'Shop',
                 'href'=>'shop'
             ]
+        ],
+        'footer_menu' => [
+            'DC Comics' => [
+                [
+                    'label' => 'Characters',
+                    'href' => 'characters'
+                ],
+                [
+                    'label' => 'Comics',
+                    'href' => 'comics'
+                ],
+                [
+                    'label' => 'Movies',
+                    'href' => 'movies'
+                ],
+                [
+                    'label' => 'TV',
+                    'href' => 'tv'
+                ],
+                [
+                    'label' => 'Games',
+                    'href' => 'games'
+                ],
+                [
+                    'label' => 'Videos',
+                    'href' => 'videos'
+                ],
+                [
+                    'label' => 'News',
+                    'href' => 'news'
+                ],
+            ],
+            'Shop' => [
+                [
+                    'label' => 'Shop DC',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Shop DC Collectibles',
+                    'href' => ''
+                ]
+            ],
+            'DC' => [
+                [
+                    'label' => 'Terms of Use',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Privacy Policy (new)',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'AD Choices',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Advertising',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Jobs',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Subscriptions',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Talent Workshops',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'CPSC Certificates',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Ratings',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Shop Help',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'Contact Us',
+                    'href' => ''
+                ]
+            ],
+            'Sites' => [
+                [
+                    'label' => 'DC',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'MAD Magazines',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'DC Kids',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'DC Universe',
+                    'href' => ''
+                ],
+                [
+                    'label' => 'DC Power Visa',
+                    'href' => ''
+                ]
+            ]
         ]
     ];
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,3 +1,51 @@
-{{-- <footer>
-    <h1>footer</h1>
-</footer> --}}
+@php
+    $footer_menu = config('menus.footer_menu');
+@endphp
+
+<footer>
+    <section class="ms-cta">
+        <div class="container d-flex justify-content-between p-5">
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="Digital Comics">
+                <span>Digital Comics</span>
+            </a>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="DC Merchandise">
+                <span>DC Merchandise</span>
+            </a>
+            <a href="#">
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="Subscription">
+                <span>Subscription</span>
+            </a>
+            <a href="#">
+                <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="Comic Shop Locator">
+                <span>Comic Shop Locator</span>
+            </a>
+            <a href="#">
+                <img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="DC Power Visa">
+                <span>DC Power Visa</span>
+            </a>
+        </div>
+    </section>
+    <section class="ms-footer-menu">
+        <div class="container d-flex justify-content-around align-items-center py-5">
+            <nav class="d-flex flex-column flex-wrap align-content-start">
+                @foreach ($footer_menu as $key => $menu)
+                <div class="ms-menu my-2 mx-3 h-auto">
+                    <h3 class="my-2">{{ $key }}</h3>
+                    <ul class="p-0">
+                        @foreach ($menu as $item)
+                            <li>
+                                <a href="{{ $item['href'] }}">{{ $item['label'] }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endforeach
+            </nav>
+            <div class="logo">
+                <!-- <img src="../../assets/img/dc-logo-bg.png" alt=""> -->
+            </div>
+        </div>
+    </section>
+</footer>
