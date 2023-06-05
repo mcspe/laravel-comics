8faed387e6f53d43c563cf92db469f5e7964fc9c
mcspe <mcspezzaferro@gmail.com>
2023-06-05 16:12:30 +0200

    [add] layout

---
+++ config/menus.php
@@ -0,0 +1,45 @@
+<?php
+    return [
+        'header_menu' => [
+            [
+                'label'=>'Characters',
+                'href'=>'characters'
+            ],
+            [
+                'label'=>'Comics',
+                'href'=>'comics'
+            ],
+            [
+                'label'=>'Movies',
+                'href'=>'movies'
+            ],
+            [
+                'label'=>'TV',
+                'href'=>'tv'
+            ],
+            [
+                'label'=>'Games',
+                'href'=>'games'
+            ],
+            [
+                'label'=>'Collectibles',
+                'href'=>'collectibles'
+            ],
+            [
+                'label'=>'Videos',
+                'href'=>'videos'
+            ],
+            [
+                'label'=>'Fans',
+                'href'=>'fans'
+            ],
+            [
+                'label'=>'News',
+                'href'=>'news'
+            ],
+            [
+                'label'=>'Shop',
+                'href'=>'shop'
+            ]
+        ]
+    ];
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Comics | @yield('title')</title>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    @include('layouts.partials.header')
+    @yield('content')
+    @include('layouts.partials.footer')
+</body>
+
+</html>
